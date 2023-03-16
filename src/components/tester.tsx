5a85b98f9b23193eb2c2cb525d588889e3b60334
klakhov <kirill.9992@gmail.com>
2023-03-16 12:16:06 +0300

    added a courpe of listeners

--- src/components/tester.tsx
+++ src/components/tester.tsx
@@ -6,14 +6,20 @@ interface Props {
 
 export default function Tester(props: Props): JSX.Element {
     const { id } = props;
+
     return (
         <div>
             Hello im tester #{id}
             <button onClick={() => {
                 window.alert(`Hello $${id}`);
-            }}>Click</button>
-            <input type="text" name="name" />
-            <input type="submit" value="Submit" />
+            }}
+            >Click</button>
+            <input type="text" name="name"  onChange={(e) => {
+                console.log(e);
+            }}/>
+            <input type="submit" value="Submit"  onClick={(e) => {
+                console.log(e);
+            }}/>
         </div>
     )
 }
