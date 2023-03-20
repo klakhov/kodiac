da38fbb7e39e31f6a3b23659de1385ec8092f950
klakhov <kirill.9992@gmail.com>
2023-03-20 16:11:14 +0300

    change amount of components to 15k

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Tester from '@/components/tester';
 import TesterStatic from '@/components/tester-static';
 
 export default function Home() {
-  const number = 30000;
+  const number = 15000;
   const elems = [];
   
   for (let i=0; i<number; i++) {
