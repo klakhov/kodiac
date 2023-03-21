ce9da471b1ad4653b9725aa5117072e9b8dc4c35
klakhov <kirill.9992@gmail.com>
2023-03-21 11:39:56 +0300

    upgrade to 5000

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Tester from '@/components/tester';
 import TesterStatic from '@/components/tester-static';
 
 export default function Home() {
-  const number = 2500;
+  const number = 5000;
   const elems = [];
   
   for (let i=0; i<number; i++) {
