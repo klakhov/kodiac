238eeaa9f2435e2fc48531904987d7533a19ba26
klakhov <kirill.9992@gmail.com>
2023-05-26 10:02:40 +0300

    pushed to 1000-75-25

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,11 @@ import Tester from '@/components/tester';
 import TesterStatic from '@/components/tester-static';
 
 export default function Home() {
-  const number = 2500;
+  const number = 1000;
   const elems = [];
   
   for (let i=0; i<number; i++) {
-    i%4==0 ? elems.push(<Tester id={i} key={i} />) : elems.push(<TesterStatic id={i} key={i} />) 
+    i%4!=0 ? elems.push(<Tester id={i} key={i} />) : elems.push(<TesterStatic id={i} key={i} />) 
   }
   return (
     <>
